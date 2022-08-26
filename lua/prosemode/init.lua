e3cb21abf8d27cb68c55c730f21d0184d90fa14c
Liam Timms <timms5000@gmail.com>
2022-08-26 14:35:23 -0400

    initial commit

---
+++ lua/prosemode/init.lua
@@ -0,0 +1,142 @@
+local M = {}
+local bkeymap = vim.api.nvim_buf_set_keymap
+local bdelete = vim.api.nvim_buf_del_keymap
+local opt = vim.opt
+local cmd = vim.api.nvim_create_user_command
+
+-- lots of reference to https://www.youtube.com/watch?v=n4Lp4cV8YR0&t=1574s
+-- please go watch the tutorial for more info
+
+local function setup_commands()
+	-- setup nvim commands user can call
+	-- based on bufferline
+	cmd("ProseToggle", function()
+		M.ProseToggle()
+	end, {})
+	cmd("ProseOn", function()
+		M.ProseOn()
+	end, {})
+	cmd("ProseOff", function()
+		M.ProseOff()
+	end, {})
+end
+
+local find_mapping = function(maps, lhs)
+	--    iteratres over ONLY numeric keys in a table
+	--    order IS guaranteed
+	for _, value in ipairs(maps) do
+		if value.lhs == lhs then
+			return value
+		end
+	end
+end
+
+M.setup = function()
+	-- create global variable to store state of prose mode
+	vim.g.prose_mode = false
+	setup_commands()
+end
+
+M._key_stack = {}
+
+M.push_keys = function(name, mode, mappings)
+	-- add keymappings for the mode to stack
+	local bmaps = vim.api.nvim_buf_get_keymap(0, mode)
+
+	local existing_maps = {}
+	for lhs, _ in pairs(mappings) do
+		local existing = find_mapping(bmaps, lhs)
+		if existing then
+			existing_maps[lhs] = existing
+		end
+	end
+
+	for lhs, rhs in pairs(mappings) do
+		bkeymap(0, mode, lhs, rhs, { noremap = true, silent = true })
+	end
+
+	M._key_stack[name] = M._key_stack[name] or {}
+
+	M._key_stack[name][mode] = {
+		existing = existing_maps,
+		mappings = mappings,
+	}
+end
+
+M._opt_stack = {}
+
+M.pop_keys = function(name, mode)
+	local state = M._key_stack[name][mode]
+	M._key_stack[name][mode] = nil
+
+	for lhs in pairs(state.mappings) do
+		if state.existing[lhs] then
+			-- Handle mappings that existed
+			local og_mapping = state.existing[lhs]
+
+			-- TODO: Handle the options from the table
+			bkeymap(0, mode, lhs, og_mapping.rhs)
+		else
+			-- Handled mappings that didn't exist
+			bdelete(0, mode, lhs)
+		end
+	end
+end
+
+M._clear = function()
+	M._key_stack = {}
+	M._opt_stack = {}
+end
+
+M.ProseOn = function()
+	M.push_keys("prose", "n", {
+		["j"] = "gj",
+		["k"] = "gk",
+		["0"] = "g0",
+		["$"] = "g$",
+		["A"] = "g$a",
+		["I"] = "g0i",
+	})
+
+	-- store the orignal settings
+	M._opt_stack = {
+		["linebreak"] = opt.linebreak:get(),
+		["wrap"] = opt.wrap:get(),
+		["number"] = opt.number:get(),
+		["relativenumber"] = opt.relativenumber:get(),
+		["foldcolumn"] = opt.foldcolumn:get(),
+	}
+
+	-- change the current settings
+	opt.linebreak = true
+	opt.wrap = true
+	opt.number = false
+	opt.relativenumber = false
+	opt.foldcolumn = 2
+
+	vim.g.prose_mode = true
+end
+
+M.ProseOff = function()
+	-- remove the keymappings we changed
+	M.pop_keys("prose", "n")
+
+	-- restore the original settings
+	opt.linebreak = M._opt_stack["linebreak"]
+	opt.wrap = M._opt_stack["wrap"]
+	opt.number = M._opt_stack["number"]
+	opt.relativenumber = M._opt_stack["relativenumber"]
+	opt.foldcolumn = M._opt_stack["foldcolumn"]
+
+	vim.g.prose_mode = false
+end
+
+M.ProseToggle = function()
+	if vim.g.prose_mode then
+		M.ProseOff()
+	else
+		M.ProseOn()
+	end
+end
+
+return M
