92ad8a3a4437c2c601fde013f7f71cec19681576
Liam Timms <timms5000@gmail.com>
2022-08-26 22:56:31 -0400

    fixed split related bugs

--- lua/prosemode/init.lua
+++ lua/prosemode/init.lua
@@ -9,7 +9,6 @@ M._opt_stack = {}
 
 M._clear = function()
 	M._key_stack = {}
-	M._opt_stack = {}
 end
 
 -- lots of reference to https://www.youtube.com/watch?v=n4Lp4cV8YR0&t=1574s
@@ -25,6 +24,23 @@ local find_mapping = function(maps, lhs)
 	end
 end
 
+local function find_existing(maps, lhs)
+	-- check if already set
+	-- avoid index errors with nested if statements
+	if M._key_stack["prose"] ~= nil then
+		if M._key_stack["prose"]["n"] ~= nil then
+			if M._key_stack["prose"]["n"].existing ~= nil then
+				local existing = M._key_stack["prose"]["n"].existing[lhs]
+				return existing
+			end
+		end
+	end
+
+	-- if not found, in existing stack then find in maps
+	local existing = find_mapping(maps, lhs)
+	return existing
+end
+
 M.push_keys = function(name, mode, mappings)
 	-- add keymappings for the mode to stack
 	local maps = vim.api.nvim_get_keymap(mode)
@@ -32,9 +48,8 @@ M.push_keys = function(name, mode, mappings)
 	-- store existing mappings
 	local existing_maps = {}
 	for lhs, _ in pairs(mappings) do
-		-- avoid overwriting if called again
-		local existing = find_mapping(maps, lhs)
-		if existing then
+		local existing = find_existing(maps, lhs)
+		if existing ~= nil then
 			existing_maps[lhs] = existing
 		end
 	end
@@ -60,7 +75,6 @@ M.pop_keys = function(name, mode)
 			-- Handle mappings that existed
 			local og_mapping = state.existing[lhs]
 
-			-- TODO: Handle the options from the table
 			keymap(mode, lhs, og_mapping.rhs, { noremap = true, silent = true })
 		else
 			-- Handled mappings that didn't exist
@@ -104,21 +118,28 @@ M.ProseOn = function()
 end
 
 M.ProseOff = function()
-	if vim.g.prose_mode then
-		-- remove the keymappings we changed
-		M.pop_keys("prose", "n")
-
-		-- restore the original settings if they were stored
-		if next(M._opt_stack) ~= nil then
-			opt.linebreak = M._opt_stack["linebreak"]
-			opt.wrap = M._opt_stack["wrap"]
-			opt.number = M._opt_stack["number"]
-			opt.relativenumber = M._opt_stack["relativenumber"]
-			opt.foldcolumn = M._opt_stack["foldcolumn"]
+	-- remove the keymappings we changed
+	if M._key_stack["prose"] ~= nil then
+		if M._key_stack["prose"]["n"] ~= nil then
+			-- avoid index errors with nested if statements
+			if M._key_stack["prose"]["n"].mappings ~= nil then
+				M.pop_keys("prose", "n")
+			end
 		end
-
-		vim.g.prose_mode = false
 	end
+
+	-- restore the original settings if they were stored
+	if next(M._opt_stack) ~= nil then
+		opt.linebreak = M._opt_stack["linebreak"]
+		opt.wrap = M._opt_stack["wrap"]
+		opt.number = M._opt_stack["number"]
+		opt.relativenumber = M._opt_stack["relativenumber"]
+		opt.foldcolumn = M._opt_stack["foldcolumn"]
+	end
+
+	M._clear()
+
+	vim.g.prose_mode = false
 end
 
 M.ProseToggle = function()
